5b958de97da8fede70d696c0d593c635c0bf7cb7
bobojonov-sobirjon <sobirbobojonov05763104@gmail.com>
2025-12-09 10:18:47 +0500

    changed resed sms code to phone api

--- apps/v1/product/views.py
+++ apps/v1/product/views.py
@@ -2008,7 +2008,32 @@ class CalculatePaymentScheduleView(APIView):
                                     except Exception as e:
                                         pass
                         else:
-                            application_status = None
+                            grist_product_ids = get_grist_product_ids_from_request(product_list)
+                            
+                            from apps.v1.order.integrations.advanced_payment_assessment import get_product_ids_from_price_table_by_grist_ids
+                            product_ids_for_application = get_product_ids_from_price_table_by_grist_ids(grist_product_ids)
+                            
+                            risk_category_id = get_risk_category_id_from_applications(applications, counterparty_id)
+                            
+                            try:
+                                current_date_str = datetime.now().strftime("%Y-%m-%d")
+                                
+                                total_advance_payment_mode2 = sum(item.get('advance_payment', 0) for item in product_list)
+                                
+                                result = post_to_grist_application(
+                                    counterparty_id=counterparty_id,
+                                    date=current_date_str,
+                                    stage='New',
+                                    risk_category_id=risk_category_id,
+                                    issue_limit=float(total_advance_payment_mode2),
+                                    products=product_ids_for_application
+                                )
+                                
+                                if result:
+                                    application_status = 'New'
+                            except Exception as e:
+                                pass
+                            
                             minimum_contribution = 0
                 except Exception:
                     application_status = None
